2d4ad7211ff73bb8864ad2f11880df1e88a76739
Daniel Becerra <daniel.becerra@estudiants.urv.cat>
2023-05-12 19:44:23 +0200

    part 1 tot ok

--- eval/single_node.py
+++ eval/single_node.py
@@ -25,20 +25,16 @@ if __name__ == '__main__':
         test1 = SimpleKVStoreTests(master_address, 1)
         test1.test()
 
-        
-
-    except KeyboardInterrupt:
-        pass
-    
-    '''
         print("-Parallel tests-")
         test2 = SimpleKVStoreParallelTests(master_address, NUM_CLIENTS)
         test2.test()
-
+        
         print("-Race condition tests-")
         test2 = SimpleKVStoreRaceTests(master_address, NUM_CLIENTS)
         test2.test()
-    '''
+
+    except KeyboardInterrupt:
+        pass
 
     print("\n\n...Terminating server")
     server_proc.terminate()
